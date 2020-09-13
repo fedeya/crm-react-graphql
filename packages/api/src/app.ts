3ba531b537b47c7c2af77b3c26f75932259cc400
Federico Minaya <elfedeminaya@gmail.com>
2020-09-12 21:11:28 -0300

    fix: fixed error for run typescript files compiled (#1)

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -9,7 +9,7 @@ export async function initServer() {
   try {
     const server = new ApolloServer({
       schema: await buildSchema({
-        resolvers: [__dirname + '/graphql/resolvers/**/*.ts'],
+        resolvers: [__dirname + '/graphql/resolvers/**/*.{ts,js}'],
         validate: false,
         authChecker
       }),
